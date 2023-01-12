1441b5ee206fce12991ea943ffa36d29285eb4d5
Jean-Simon Lhost <jean.si@hotmail.fr>
2023-01-12 16:04:15 +0100

    first commit

---
+++ day_1/main.py
@@ -0,0 +1,21 @@
+if __name__ == "__main__":
+    
+    import pandas as pd
+
+    with open('input.txt') as file:
+        data = file.read()
+
+    data_liste = data.split('\n')
+    df = pd.DataFrame(data_liste, columns=['calories'])
+    hole_index = df.loc[df['calories']==""].index
+
+    base_index = 0
+    calories_sum_list = []
+
+    for idx in hole_index:
+        elf_carrying = df.iloc[base_index:idx].astype("int")
+        calories_sum = elf_carrying.sum().values[0]
+        calories_sum_list.append(calories_sum)
+        base_index = idx + 1
+
+    print(max(calories_sum_list))
---
+++ day_2/main.py
@@ -0,0 +1,50 @@
+if __name__ == "__main__":
+
+    import pandas as pd
+    df = pd.read_csv("input.txt", sep=" ", header=None)
+
+    opponent_shape = {'A' : 1, 'B' : 2, 'C' : 3}
+    my_shape = {'X' : 1, 'Y' : 2, 'Z' : 3}
+    conclusion = {'lost' : 0, 'draw' : 3, 'win' : 6}
+    losing = [1, -2]
+    winning = [-1, 2]
+    score = 0
+
+    for idx in df.index:
+        diff_score = opponent_shape[df.iloc[idx,0]] - my_shape[df.iloc[idx,1]]
+
+        if diff_score in losing:
+            score += conclusion['lost'] + my_shape[df.iloc[idx,1]]
+        elif diff_score in winning:
+            score += conclusion['win'] + my_shape[df.iloc[idx,1]]
+        else:
+            score += conclusion['draw'] + my_shape[df.iloc[idx,1]]
+
+    print(score)
+
+
+    import pandas as pd
+    df = pd.read_csv("input.txt", sep=" ", header=None)
+
+    opponent_shape = {'A' : 1, 'B' : 2, 'C' : 3}
+    conclusion = {'lost' : 0, 'draw' : 3, 'win' : 6}
+    losing = [1, -2]
+    winning = [-1, 2]
+    shape = [1, 2, 3]
+    score= 0
+
+    for idx in df.index:
+            if df.iloc[idx,1] == 'X': #lost
+                    for score_shape in shape:
+                            diff = opponent_shape[df.iloc[idx,0]] - score_shape
+                            if  diff in losing:
+                                    score += conclusion['lost'] + score_shape
+            elif df.iloc[idx,1] == 'Y': # draw
+                    score += conclusion['draw'] + opponent_shape[df.iloc[idx,0]]
+            elif df.iloc[idx,1] == 'Z': # win
+                    for score_shape in shape:
+                            diff = opponent_shape[df.iloc[idx,0]] - score_shape
+                            if  diff in winning:
+                                    score += conclusion['win'] + score_shape
+
+    print(score)
