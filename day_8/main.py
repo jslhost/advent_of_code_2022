dde1cd40932246a781f882c0c7017657dc948e73
Jean-Simon Lhost <jean.si@hotmail.fr>
2023-01-20 20:10:10 +0100

    Adding part 1 of day 8

---
+++ day_8/main.py
@@ -0,0 +1,41 @@
+if __name__ == '__main__':
+    import numpy as np
+    import pandas as pd
+
+    with open('input.txt') as file:
+        data = file.read().splitlines()
+
+    forest = np.array([[int(tree) for tree in row] for row in data])
+    df = pd.DataFrame(data=forest)
+
+    # Part 1 ---------------------------------------------------------------
+    nb_bordures = (df.shape[0]-1) * 4
+    visible_tree_count = 0
+
+    for col in range(1, 98):
+        for row in range(1, 98):
+
+            list_of_directions = [
+                df.iloc[0:row, col], 
+                df.iloc[row+1:None, col][::-1], 
+                df.iloc[row, 0:col], 
+                df.iloc[row, col+1:None][::-1]
+                ]
+            target_tree = df.iloc[row, col]
+            visible_tree_angle = 0
+
+            for direction in list_of_directions:
+                tree_is_visible = True
+                for tree in direction:
+                    if tree >= target_tree:
+                        tree_is_visible = False
+                        break
+                if tree_is_visible:
+                    visible_tree_angle += 1
+
+            if visible_tree_angle > 0:
+                visible_tree_count += 1
+
+    print(visible_tree_count + nb_bordures)
+
+    # Part 2 ---------------------------------------------------------------
